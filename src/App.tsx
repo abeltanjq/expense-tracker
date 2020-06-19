1178bd1e324a74e6e2d5024ba0c55e136a2a2980
abeltanjq <abeltanjq@gmail.com>
2020-06-20 00:49:59 +0800

    Remove console

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import AddItem from "./Components/AddItem";
 const App: FC = () => {
     const [expenses, setExpenses] = useState<Array<ExpenseItem>>([{name: "Maggi Mee", amount: 8.9, description: "instant noodles"}]);
     const handleNewItem = (item: ExpenseItem) => {
-        console.log(`item being added: ${item}`)
         setExpenses([
             ...expenses,
             item
@@ -16,10 +15,12 @@ const App: FC = () => {
         <div className="App">
             <AddItem onNewItemAdded={handleNewItem} />
             <ul>
-                {expenses.map(item => <ExpenseItem expenseItem={item} />)}
+                {expenses.map(item => <ExpenseItem key={Math.random()} expenseItem={item} />)}
             </ul>
         </div>
     );
 }
+
+// todo: Key of list should be an uuid v4
 
 export default App;
